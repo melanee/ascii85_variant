62d811bf4b691ca727f2a26de23691197f944be2
melanee <melanee@tutanota.de>
2020-05-04 16:44:29 -0400

    add x85ml.py and x85sl.py locally

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL V03 License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
